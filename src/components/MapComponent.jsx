af8fb131d08f52616512d5a7b854d2a6ad32dcc1
Pantonym <nicojvanwyk03@gmail.com>
2024-12-03 15:08:02 +0200

    Updated Maps

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,28 +1,100 @@
-import React from 'react';
-import { MapContainer, ImageOverlay } from 'react-leaflet';
+import React, { useState } from 'react';
+import { MapContainer, ImageOverlay, LayersControl } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
-const bounds = [[0, 0], [2570.56, 4000]];
+const bounds = [[0, 0], [5111, 6809]];
 
 const MapComponent = () => {
+    // State for managing overlays and base maps
+    const [regionOverlayVisible, setRegionOverlayVisible] = useState(true);
+    const [regionOpacity, setRegionOpacity] = useState(0.5);
+    const [baseMap, setBaseMap] = useState("countryNames"); // Default base map
+
+    // URLs for base maps
+    const baseMapUrls = {
+        countryNames: "https://firebasestorage.googleapis.com/v0/b/lorekeeper-6ffd8.appspot.com/o/maps%2FCountries.png?alt=media&token=cbbae2e0-64a1-41d9-b037-8f547dec649c",
+        landmarksCities: "https://firebasestorage.googleapis.com/v0/b/lorekeeper-6ffd8.appspot.com/o/maps%2FLandmarksAndCities.png?alt=media&token=d4912efe-ec30-4c03-9e10-6967a5de8fa8",
+        combined: "https://firebasestorage.googleapis.com/v0/b/lorekeeper-6ffd8.appspot.com/o/maps%2FCombined.png?alt=media&token=45b23859-2f26-46c9-a9d7-1cba49740d98"
+    };
+
+    // URL for the regions overlay
+    const regionOverlayUrl = "https://firebasestorage.googleapis.com/v0/b/lorekeeper-6ffd8.appspot.com/o/maps%2FRegions.png?alt=media&token=4efae0ca-826f-4770-941a-77e2ff1166ab";
+
     return (
-        <MapContainer
-            center={[1285.28, 2000]}
-            zoom={0}
-            minZoom={-5}
-            maxZoom={2}
-            style={{ height: '100vh', width: '80%', alignSelf: 'center', marginTop: '20px', zIndex: '0' }}
-            crs={L.CRS.Simple}
-        >
-            <ImageOverlay
-                url="https://firebasestorage.googleapis.com/v0/b/lorekeeper-6ffd8.appspot.com/o/WesternIrea.jpg?alt=media&token=4248d769-b943-4f04-9469-b16d4ac2bf42"
-                bounds={bounds}
-            />
-        </MapContainer>
+        <div>
+            {/* Control Panel */}
+            <div style={{ padding: '10px', background: '#f5f5f5', borderBottom: '1px solid #ddd' }}>
+                <h2>Map Controls</h2>
+
+                {/* Base Map Selection */}
+                <label>
+                    Base Map:
+                    <select
+                        value={baseMap}
+                        onChange={(e) => setBaseMap(e.target.value)}
+                        style={{ marginLeft: '10px' }}
+                    >
+                        <option value="countryNames">Country Names</option>
+                        <option value="landmarksCities">Landmarks & Cities</option>
+                        <option value="combined">Both</option>
+                    </select>
+                </label>
+
+                {/* Toggle Region Overlay */}
+                <label style={{ marginLeft: '20px' }}>
+                    Show Region Overlay
+                    <input
+                        type="checkbox"
+                        checked={regionOverlayVisible}
+                        onChange={(e) => setRegionOverlayVisible(e.target.checked)}
+                    />
+                </label>
+
+                {/* Slider for Region Overlay Opacity */}
+                {regionOverlayVisible && (
+                    <div style={{ marginTop: '10px' }}>
+                        <label>Region Overlay Opacity:</label>
+                        <input
+                            type="range"
+                            min="0"
+                            max="1"
+                            step="0.01"
+                            value={regionOpacity}
+                            onChange={(e) => setRegionOpacity(e.target.value)}
+                            style={{ marginLeft: '10px', width: '200px' }}
+                        />
+                        <span style={{ marginLeft: '10px' }}>{Math.round(regionOpacity * 100)}%</span>
+                    </div>
+                )}
+            </div>
+
+            {/* Map */}
+            <MapContainer
+                center={[1285.28, 2000]}
+                zoom={0}
+                minZoom={-3}
+                maxZoom={2}
+                style={{ height: 'calc(100vh - 100px)', width: '100%' }}
+                crs={L.CRS.Simple}
+            >
+                {/* Base Map */}
+                <ImageOverlay
+                    url={baseMapUrls[baseMap]}
+                    bounds={bounds}
+                />
+
+                {/* Region Overlay */}
+                {regionOverlayVisible && (
+                    <ImageOverlay
+                        url={regionOverlayUrl}
+                        bounds={bounds}
+                        opacity={regionOpacity} // Set opacity dynamically
+                    />
+                )}
+            </MapContainer>
+        </div>
     );
 };
 
 export default MapComponent;
-
-// TODO: Map editor
--- src/pages/MapsPage.jsx
+++ src/pages/MapsPage.jsx
@@ -1,11 +1,49 @@
 import React from 'react';
 import MapComponent from '../components/MapComponent';
+// Icons
+import CapitalCity from '../assets/Legend/Icons/CapitalCity.png';
+import CityVillage from '../assets/Legend/Icons/City&Village.png';
+import Fort from '../assets/Legend/Icons/Fort.png';
+import Landmark from '../assets/Legend/Icons/Landmark.png';
+// Aliances & Empires
+import EasternFenHisan from '../assets/Legend/Alliances&Empires/EasternFenHisanEmpire.png';
+import FreeStates from '../assets/Legend/Alliances&Empires/FreeStates.png';
+import DalwinianAlliance from '../assets/Legend/Alliances&Empires/DalwinianAlliance.png';
+import Hillgrave from '../assets/Legend/Alliances&Empires/HillgraveAlliance.png';
+import Twins from '../assets/Legend/Alliances&Empires/TheTwins.png';
+import Ardor from '../assets/Legend/Alliances&Empires/Ardor.png';
+import Havaria from '../assets/Legend/Alliances&Empires/HavarianEmpire.png';
 
 const MapsPage = () => {
     return (
         <div className="container">
             <h1>Interactive Map</h1>
             <MapComponent />
+
+            <h2>Icons:</h2>
+            <div className='row' style={{ gap: '20px' }}>
+                <img src={CapitalCity} alt="Capital City" style={{ height: '125px' }} />
+                <img src={CityVillage} alt="City Village" style={{ height: '125px' }} />
+                <img src={Landmark} alt="Landmark" style={{ height: '125px' }} />
+                <img src={Fort} alt="Fort" style={{ height: '125px' }} />
+            </div>
+
+            <h2>Alliances/Empires:</h2>
+            <div className="row">
+                <img src={EasternFenHisan} alt="Eastern Fen'Hisan Empire" style={{ height: '175px' }} />
+                <img src={FreeStates} alt="Free City States" style={{ height: '175px' }} />
+                <img src={DalwinianAlliance} alt="Dalwinian Alliance" style={{ height: '175px' }} />
+                <img src={Hillgrave} alt="Alliance Of The Hillgraves" style={{ height: '175px' }} />
+                <img src={Twins} alt="The Twins" style={{ height: '175px' }} />
+                <img src={Ardor} alt="Ardoran Empire" style={{ height: '175px' }} />
+                <img src={Havaria} alt="Havarian Empire" style={{ height: '175px' }} />
+            </div>
+
+            <br></br>
+            <a href="https://www.flaticon.com/free-icon/houses_2109397?term=village&related_id=2109397" title="village icons">Village icons created by Freepik - Flaticon</a>
+            <a href="https://www.flaticon.com/free-icon/tower_2778395?term=fortress&page=1&position=14&origin=tag&related_id=2778395" title="fortress icons">Fortress icons created by Freepik - Flaticon</a>
+            <a href="https://www.flaticon.com/free-icon/monument_708104?term=monument&related_id=708104" title="monument icons">Monument icons created by Freepik - Flaticon</a>
+            <a href="https://www.flaticon.com/free-icons/capital" title="castle icons">Capital icons created by Freepik - Flaticon</a>
         </div>
     );
 };
